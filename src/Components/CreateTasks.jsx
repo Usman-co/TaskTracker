7521e23795d5bb4c18f37399394d2dffdcf6f8c5
Usman Ali <162821354+Usman-co@users.noreply.github.com>
2024-10-09 20:00:48 +0500

    "First commit"

--- src/Components/CreateTasks.jsx
+++ src/Components/CreateTasks.jsx
@@ -32,7 +32,7 @@ const CreateTasks = () => {
     addTasks(newTask);
   };
   return (
-    <>
+    <div>
       <form
         onSubmit={handleAddTask}
         action="/upload"
@@ -40,28 +40,31 @@ const CreateTasks = () => {
         encType="multipart/form-data"
       >
          <h1 className="mb-4 font-bold">Add Tasks</h1>
-      <div className="flex space-x-2 mb-4 ">
+      <div className="flex space-x-2 mb-4 max-sm:space-x-1 max-sm:flex-col ">
+        <div className="flex space-x-2">
         <input
           type="text"
           placeholder="Enter task name"
           ref={titleElement}
-          className=" mb-2 rounded py-1 px-2 font-normal  text-black"
+          className=" mb-2 rounded py-1 px-2  font-normal  text-black max-sm:w-1/2"
         />
         <input
           type="number"
           placeholder="Planned time (minutes)"
           ref={durationElement}
-          className=" mb-2 rounded py-1 px-2 font-normal  text-black"
+          className=" mb-2 rounded py-1 px-2 font-normal  text-black max-sm:w-1/2"
         />
-        <button className="bg-black text-white mt-0 flex py-2 px-4 rounded-md ">
-          <p className="h-4 w-4 mr-2 mt-1 ">
+        </div>
+        
+        <button className="bg-black text-white mt-0 flex py-2 px-4 rounded-md max-sm:w-1/4  ">
+          <p className="h-4 w-4 mr-2 mt-1 max-sm:h-2 max-sm:w-2 ">
             <IoMdAdd />
           </p>
           Add Task
         </button>
       </div>
       </form>
-    </>
+    </div>
   );
 };
 
